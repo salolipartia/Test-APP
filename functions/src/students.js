68d58e669d5fdfdce0f91403c15836237bb4c975
salolipartia <salome.lipartia.1@btu.edu.ge>
2020-02-25 23:12:39 +0400

    1. Created score's GET, PUT and POST requests 2. Created GET request to get average score by student's id

--- functions/src/students.js
+++ functions/src/students.js
@@ -53,8 +53,7 @@ app.post('/', (req, res) => {
             firstName: firstName || null,
             lastName: lastName || null,
             birthDate: date,
-            address: address || null,
-            score: score || null
+            address: address || null
         }
 
         try {
@@ -105,4 +104,87 @@ app.get('/:id/:field', (req, res) => {
         return res.status(500).send(error)
     }
 })
+
+app.get('/scores', (req, res) => {
+    db.collection('scores').get().then(snapshot => {
+        const data = []
+        snapshot.forEach(doc => {
+            data.push(doc.data())
+        })
+        return res.status(200).send(data)
+    }).catch(error => {
+        console.log(error)
+        return res.status(500).send(error)
+    })
+})
+
+app.post('/scores', (req, res) => {
+    const {
+        studentId,
+        score
+    } = req.body
+
+    const scores = {
+        studentId: studentId || null,
+        score: score || null
+
+    }
+
+    try {
+        db.collection('scores').doc(studentId).set(scores)
+        return res.status(200).send(scores)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+
+})
+
+app.put('/scores/:id', (req, res) => {
+    try {
+        db.collection('scores').doc(`${req.params.id}`).update(req.body).then(() => {
+            db.collection('scores').doc(`${req.params.id}`).get().then(doc => {
+                if (!doc) {
+                    res.status(400).send('Does Not Exist')
+                } else {
+                    res.status(200).send(doc.data())
+                }
+            })
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+})
+
+app.get('/scores/average/:id', (req, res) => {
+    try {
+        db.collection('scores').doc(`${req.params.id}`).get().then(doc => {
+            if (!doc) {
+                res.status(400).send('Does Not Exist')
+            } else {
+
+                const { score } = doc.data()
+
+                const length = score.length
+                let sum = 0
+
+                for (let i = 0; i < length; i++) {
+                    sum += parseInt(score[i])
+                }
+
+                const average = sum / length
+
+                res.status(200).send(`${average}`)
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+})
+
 module.exports = app
