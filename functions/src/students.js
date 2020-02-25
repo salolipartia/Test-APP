a264c5392a63dc889ea703eec733f81d9092a5d0
salolipartia <salome.lipartia.1@btu.edu.ge>
2020-02-25 21:25:53 +0400

    Created GET request to get a specific field by id

--- functions/src/students.js
+++ functions/src/students.js
@@ -4,88 +4,105 @@ const app = express()
 const db = admin.firestore()
 
 const getCurrentStudent = (id, callback) => {
-  if (id) {
-    db.collection('students').doc(`${id}`).get().then(doc => {
-      callback(doc)
-    })
-  }
+    if (id) {
+        db.collection('students').doc(`${id}`).get().then(doc => {
+            callback(doc)
+        })
+    }
 }
 
 app.get('/', (req, res) => {
-  db.collection('students').get().then(snapshot => {
-    const data = []
-    snapshot.forEach(doc => {
-      data.push(doc.data())
+    db.collection('students').get().then(snapshot => {
+        const data = []
+        snapshot.forEach(doc => {
+            data.push(doc.data())
+        })
+        return res.status(200).send(data)
+    }).catch(error => {
+        console.log(error)
+        return res.status(500).send(error)
     })
-    return res.status(200).send(data)
-  }).catch(error => {
-    console.log(error)
-    return res.status(500).send(error)
-  })
 })
 
 app.get('/:id', (req, res) => {
-  const id = req.params.id || null
-  getCurrentStudent(id, (doc) => {
-    if (!doc) {
-      res.status(400).send('Does Not Exist')
-    } else {
-      res.status(200).send(doc.data())
-    }
-  })
+    const id = req.params.id || null
+    getCurrentStudent(id, (doc) => {
+        if (!doc) {
+            res.status(400).send('Does Not Exist')
+        } else {
+            res.status(200).send(doc.data())
+        }
+    })
 })
 
 app.post('/', (req, res) => {
-  const {
-    firstName,
-    lastName,
-    birthDate,
-    address,
-    score
-  } = req.body
+    const {
+        firstName,
+        lastName,
+        birthDate,
+        address,
+        score
+    } = req.body
 
-  const date = birthDate ? admin.firestore.Timestamp.fromDate(new Date(birthDate)) : null
+    const date = birthDate ? admin.firestore.Timestamp.fromDate(new Date(birthDate)) : null
 
-  db.collection('students').get().then(snap => {
+    db.collection('students').get().then(snap => {
 
-    const student = {
-      id: snap.size + 1,
-      firstName: firstName || null,
-      lastName: lastName || null,
-      birthDate: date,
-      address: address || null,
-      score: score || null
-    }
+        const student = {
+            id: snap.size + 1,
+            firstName: firstName || null,
+            lastName: lastName || null,
+            birthDate: date,
+            address: address || null,
+            score: score || null
+        }
 
-    try {
-      db.collection('students').doc(`${snap.size + 1}`).set(student)
-      return res.status(200).send(student)
-    } catch (error) {
-      console.log(error)
-      return res.status(500).send(error)
-    }
-  }).catch(error => {
-    console.log(error)
-    return res.status(500).send(error)
-  })
+        try {
+            db.collection('students').doc(`${snap.size + 1}`).set(student)
+            return res.status(200).send(student)
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send(error)
+        }
+    }).catch(error => {
+        console.log(error)
+        return res.status(500).send(error)
+    })
 
 
 
 })
 
 app.put('/:id', (req, res) => {
-  try {
-    db.collection('students').doc(`${req.params.id}`).update(req.body)
-    getCurrentStudent(req.params.id, (doc) => {
-      if (!doc) {
-        res.status(400).send('Does Not Exist')
-      } else {
-        res.status(200).send(doc.data())
-      }
-    })
-  } catch (error) {
-    console.log(error)
-    return res.status(500).send(error)
-  }
+    try {
+        db.collection('students').doc(`${req.params.id}`).update(req.body)
+        getCurrentStudent(req.params.id, (doc) => {
+            if (!doc) {
+                res.status(400).send('Does Not Exist')
+            } else {
+                res.status(200).send(doc.data())
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+})
+
+app.get('/:id/:field', (req, res) => {
+    try {
+        const field = req.params.field
+        getCurrentStudent(req.params.id, (doc) => {
+            if (!doc) {
+                res.status(400).send('Does Not Exist')
+            } else {
+                const user = doc.data()
+                res.status(200).send(user[field])
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
 })
 module.exports = app
